4cc4b8cb467044309c3b49ff99852f7248770125
Gwendalynn Lim Wan Ting <gwenlynn.lim@gmail.com>
2026-01-10 04:48:51 +0000

    please ceck // Import the functions you need from the SDKs you need impo

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-6085597524-b089a",
-  "appId": "1:566455898706:web:1745da1ee5029283edc58b",
   "apiKey": "AIzaSyApmyhB7HDuuzq-3FCnhlwmeIRaxocSOwU",
   "authDomain": "studio-6085597524-b089a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "566455898706"
+  "projectId": "studio-6085597524-b089a",
+  "storageBucket": "studio-6085597524-b089a.firebasestorage.app",
+  "messagingSenderId": "566455898706",
+  "appId": "1:566455898706:web:1745da1ee5029283edc58b",
+  "measurementId": "G-JV2BJTGMK0"
 };
